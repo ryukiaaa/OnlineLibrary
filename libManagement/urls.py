f8fc2f31a49824af140cf50ca6f4b7c202e2a67e
Busarang <riyukiara@gmail.com>
2024-11-25 03:46:19 +0800

    Integrated Book Detail page
    connect urls in landing page to each individual book detail page

--- libManagement/urls.py
+++ libManagement/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("books/", views.books, name="books")
+    path("books/", views.books, name="books"),
+    path('book/<int:id>/', views.book, name='book-detail'),
 ]
